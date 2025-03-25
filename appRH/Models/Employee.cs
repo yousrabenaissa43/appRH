be9fa7a4611e7ffc734208139c0f8968ee3e172b
yousrabenaissa43 <yousrabenaissa43@gmail.com>
2025-03-25 03:36:28 +0100

    updated employee model

--- appRH/Models/Employee.cs
+++ appRH/Models/Employee.cs
@@ -18,5 +18,8 @@ namespace appRH.Models
          public string Departement { get; set; }
     
          public bool isActive { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
     } 
 }
